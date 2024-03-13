7999245eb978a79ca146aaad4c4a2cbe76ec368c
artus25200 <68189043+artus25200@users.noreply.github.com>
2024-03-13 21:45:19 +0100

    int literals

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,6 +8,7 @@ typedef enum {
   T_EOF,
   T_UNKNOWN,
   T_INTLIT,
+  T_STRLIT,
   T_STAR,
   T_SLASH,
   T_PLUS,
@@ -51,6 +53,11 @@ char next() {
   return ch;
 }
 
+char previous() {
+  --c;
+  return input[c];
+}
+
 char skip() {
   char ch;
   do {
@@ -59,34 +66,52 @@ char skip() {
   return ch;
 }
 
+double parse_int(char ch) {
+  int value = 0;
+  while (isdigit(ch)) {
+    value *= 10;
+    value += ch - '0';
+    ch = next();
+  }
+  previous();
+  return value;
+}
+
 int read_tokens(token *list) {
   c = 0;
   char ch;
   int i = 0;
   while ((ch = skip()) != -1) {
     token token;
-    token.type = T_UNKNOWN;
 
     switch (ch) {
     case '\0':
       token.type = T_EOF;
+      break;
     case '+':
       token.type = T_PLUS;
+      break;
     case '-':
       token.type = T_MINUS;
+      break;
     case '*':
       token.type = T_STAR;
+      break;
     case '/':
       token.type = T_SLASH;
+      break;
     default:
-      printf("%d |", token.type);
-      if (token.type == T_UNKNOWN)
-        exit(1);
-      if (list != NULL) {
-        list[i] = token;
+      if (isdigit(ch)) {
+        token.type = T_INTLIT;
+        token.int_value = parse_int(ch);
+        break;
       }
-      ++i;
+      exit(1);
     }
+    if (list != NULL) {
+      list[i] = token;
+    }
+    ++i;
   }
   c = 0;
   return i;
@@ -101,6 +126,8 @@ int main(int argc, char **argv) {
   read_tokens(token_list);
   for (int i = 0; i < token_count; ++i) {
     printf("%d ", token_list[i].type);
+    if (token_list[i].type == T_INTLIT)
+      printf("(%d) ", token_list[i].int_value);
   }
   return 0;
 }
