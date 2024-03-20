d4c3e8a9171aaad9ae95a27648eefc540079285d
artus25200 <artus25200@gmail.com>
2024-03-20 20:52:17 +0100

    added operations and tuples

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #define MAX_IDENT_SIZE 50
+#define MAX_STR_SIZE 2000
 
 typedef enum {
   T_EOF,
@@ -21,7 +22,10 @@ typedef enum {
   T_RPAR,
   T_LBRAC,
   T_RBRAC,
-  T_PRINT
+  T_COMMA,
+  // KEYWORDS
+  T_PRINT,
+  T_NEWLINE
 } tokentype;
 
 typedef enum {
@@ -35,14 +39,16 @@ typedef enum {
   O_NEGATE,
   O_STRLIT,
   O_IDENT,
+  O_TUPLE, // arg1, arg2, arg3
   O_PRINT
 } ast_op;
 
-int op_pr[] = {0, 10, 10, 20, 20, 0, 0, 30, 0, 0};
+int op_pr[] = {0, 10, 10, 20, 20, 0, 0, 30, 0, 0, 5, 0};
 
 int op_priority(ast_op op) {
   int priority = op_pr[op];
   if (priority == 0) {
+    fprintf(stderr, "Wrong operand, op : %d\n", op);
     exit(1);
   }
   return priority;
@@ -66,7 +72,10 @@ ast_op token_to_op(tokentype type) {
     return O_IDENT;
   case T_PRINT:
     return O_PRINT;
+  case T_COMMA:
+    return O_TUPLE;
   default:
+    fprintf(stderr, "Token is not an operand : %d\n", type);
     exit(1);
   }
 }
@@ -98,21 +107,16 @@ struct parser {
   token *current_token;
   token *next_token;
 };
-typedef enum { DOUBLE_RESULT, INT_RESULT, STRING_RESULT } resultType;
-typedef struct {
-  bool isThereReturn;
+typedef enum { NUMBER, STRING, TUPLE } resultType;
+typedef struct interpretResult {
+  bool isReturn;
+  resultType type;
   union {
-    double double_value;
-    int int_value;
-    char *string_value;
+    double number;
+    char *string;
   };
-  resultType resulttype;
-  union {
-    double double_return;
-    int int_return;
-    char *string_return;
-  };
-  resultType returntype;
+  ast *tuple_left;
+  ast *tuple_right;
 } interpretResult;
 
 char *input;
@@ -153,6 +157,16 @@ char skip() {
   do {
     ch = next();
   } while (ch == ' ' || ch == '\n' || ch == '\t');
+  if (ch == '/') {
+    if ((ch = next()) == '/') {
+      while ((ch = next()) != '\n') {
+      }
+      ch = skip();
+    } else {
+      previous();
+      ch = '/';
+    }
+  }
   return ch;
 }
 
@@ -172,6 +186,9 @@ tokentype keyword(char *ident) {
   case 'p':
     if (strcmp(ident, "print") == 0)
       return T_PRINT;
+  case 'n':
+    if (strcmp(ident, "nl") == 0 || strcmp(ident, "newline") == 0)
+      return T_NEWLINE;
   }
   return T_IDENT;
 }
@@ -193,6 +210,8 @@ int read_tokens(token *list) {
   c = 0;
   char ch;
   int i = 0;
+  char ident[MAX_IDENT_SIZE];
+  char string[MAX_STR_SIZE];
   while ((ch = skip()) != -1) {
     token token;
 
@@ -202,6 +221,9 @@ int read_tokens(token *list) {
       break;
     case ';':
       token.type = T_SEMI;
+      break;
+    case ',':
+      token.type = T_COMMA;
       break;
     case '+':
       token.type = T_PLUS;
@@ -215,6 +237,34 @@ int read_tokens(token *list) {
     case '/':
       token.type = T_SLASH;
       break;
+    case '(':
+      token.type = T_LPAR;
+      break;
+    case ')':
+      token.type = T_RPAR;
+      break;
+    case '{':
+      token.type = T_LBRAC;
+      break;
+    case '}':
+      token.type = T_RBRAC;
+      break;
+    case '"': {
+      int ic = 0;
+      while ((ch = next()) != '"' && ch != EOF) {
+        if (ic >= MAX_STR_SIZE - 1) {
+          fprintf(stderr, "Max string size (2000) exceeded.\n");
+          exit(1);
+        }
+        string[ic] = ch;
+        ++ic;
+      }
+      string[ic] = '\0';
+      token.type = T_STRLIT;
+      token.ident = malloc(sizeof(char) * (strlen(string) + 1));
+      strcpy(token.ident, string);
+      break;
+    }
     default:
       if (isdigit(ch)) {
         token.type = T_INTLIT;
@@ -225,13 +275,14 @@ int read_tokens(token *list) {
         token.type = keyword(token.ident);
         break;
       }
+      fprintf(stderr, "Unknow token : %c", ch);
       exit(1);
     }
-    printf("[token %d", token.type);
-    if (token.type == T_INTLIT) {
-      printf(" value : %d", token.int_value);
-    }
-    printf("]\n");
+    /* printf("[token %d", token.type); */
+    /* if (token.type == T_INTLIT) { */
+    /*   printf(" value : %d", token.int_value); */
+    /* } */
+    /* printf("]\n"); */
     if (list != NULL) {
       list[i] = token;
     }
@@ -279,10 +330,11 @@ ast *glue_ast(ast *tree) {
 
 struct parser *parser;
 void next_token() {
+  parser->i++;
   parser->previous_token = parser->current_token;
   parser->current_token = parser->next_token;
-  if (token_list[parser->i].type != T_EOF) {
-    parser->next_token = &token_list[(parser->i + 2)];
+  if (parser->current_token->type != T_EOF) {
+    parser->next_token = &token_list[(parser->i + 1)];
   }
 }
 
@@ -296,9 +348,13 @@ void match(tokentype type, char *expected) {
 
 void rpar() { match(T_RPAR, ")"); }
 void rbrac() { match(T_RBRAC, "}"); }
+void lbrac() { match(T_LBRAC, "{"); }
 void semi() { match(T_SEMI, ";"); }
-ast *statement();
+void comma() { match(T_COMMA, ","); }
+
+ast *compound_statement(bool is_top_level);
 ast *binary_expression(int pp);
+
 ast *unary_expression() {
   ast *node;
   switch (parser->current_token->type) {
@@ -311,6 +367,10 @@ ast *unary_expression() {
         make_ast_unary(O_INTLIT, NULL, parser->current_token->int_value, NULL);
     next_token();
     break;
+  case T_NEWLINE:
+    node = make_ast_unary(O_STRLIT, NULL, 0, "\n");
+    next_token();
+    break;
   case T_STRLIT:
     node = make_ast_unary(O_STRLIT, NULL, 0, parser->current_token->ident);
     next_token();
@@ -321,14 +381,19 @@ ast *unary_expression() {
     rpar();
     break;
   case T_LBRAC:
-    next_token();
-    node = statement();
-    rbrac();
+    node = compound_statement(false);
     break;
   case T_PRINT:
     next_token();
     node = binary_expression(0);
     node = make_ast_unary(O_PRINT, node, 0, NULL);
+    while (parser->current_token->type != T_SEMI &&
+           parser->current_token->type != T_EOF) {
+      match(T_COMMA, ",");
+      node = make_ast(O_GLUE, node,
+                      make_ast_unary(O_PRINT, binary_expression(0), 0, NULL), 0,
+                      NULL);
+    }
     break;
   default:
     fprintf(stderr, "Syntax Error\n");
@@ -344,32 +409,53 @@ ast *binary_expression(int pp) {
       parser->current_token->type == T_EOF ||
       parser->current_token->type == T_RPAR)
     return left;
+  if (parser->current_token->type == T_RBRAC)
+    semi(); // will throw an error
 
   int token_type = parser->current_token->type;
-  while (op_priority(token_type) > pp) {
+  while (op_priority(token_to_op(token_type)) > pp) {
     next_token();
-    right = binary_expression(op_priority(token_type));
-    left = make_ast(token_type, left, right, 0, NULL);
+    right = binary_expression(op_priority(token_to_op(token_type)));
+    left = make_ast(token_to_op(token_type), left, right, 0, NULL);
 
     token_type = parser->current_token->type;
     if (parser->current_token->type == T_SEMI ||
         parser->current_token->type == T_EOF ||
         parser->current_token->type == T_RPAR)
       return left;
+    if (parser->current_token->type == T_RBRAC)
+      semi(); // will throw an error
   }
   return left;
 }
 
 ast *statement() {
-  ast *main_tree = malloc(sizeof(ast));
-  main_tree->op = O_GLUE;
-  ast *current_tree = main_tree;
-  while (parser->current_token->type != T_EOF || parser->current_token->type ||
-         T_RBRAC) {
-    current_tree->left = binary_expression(0);
+  ast *tree;
+  tree = binary_expression(0);
+  return tree;
+}
+
+ast *compound_statement(bool is_top_level) {
+  if (!is_top_level)
+    lbrac();
+
+  ast *main_tree = NULL;
+  ast *right = NULL;
+  while (1) {
+    if (parser->current_token->type == T_EOF ||
+        parser->current_token->type == T_RBRAC)
+      break;
+    right = statement();
     semi();
-    current_tree = glue_ast(current_tree);
+    if (right != NULL) {
+      if (main_tree == NULL)
+        main_tree = right;
+      else
+        main_tree = make_ast(O_GLUE, main_tree, right, 0, NULL);
+    }
   }
+  if (!is_top_level)
+    rbrac();
   return main_tree;
 }
 
@@ -380,65 +466,59 @@ void free_ast(ast *tree) {
     free_ast(tree->right);
   free(tree);
 }
-
+void print_result(interpretResult result);
 interpretResult interpret_ast(ast *tree) {
   interpretResult result;
+  result.isReturn = false;
+  if (tree == NULL)
+    return result;
+  interpretResult resultleft = interpret_ast(tree->left);
+  interpretResult resultright = interpret_ast(tree->right);
+  resultType lefttype = resultleft.type;
+  resultType righttype = resultright.type;
+  bool sub = false;
   switch (tree->op) {
   case O_GLUE:
-    result = interpret_ast(tree->left);
-    if (result.isThereReturn)
-      return result;
-    if (!tree->right)
-      break;
-    result = interpret_ast(tree->right);
-    if (result.isThereReturn)
-      return result;
+    if (resultleft.isReturn)
+      return resultleft;
+    if (resultright.isReturn)
+      return resultright;
     break;
   case O_INTLIT:
-    // TODO: add double support
-    result.resulttype = INT_RESULT;
-    result.int_value = (int)tree->value;
+    result.type = NUMBER;
+    result.number = tree->value;
+    break;
+  case O_STRLIT:
+    result.type = STRING;
+    result.string = tree->ident;
+    break;
+  case O_TUPLE:
+    result.type = TUPLE;
+    result.tuple_left = tree->left;
+    result.tuple_right = tree->right;
     break;
   case O_PRINT:
-    result = interpret_ast(tree->left);
-    switch (result.resulttype) {
-    case DOUBLE_RESULT:
-      printf("%f", result.double_value);
-      break;
-    case INT_RESULT:
-      printf("%d", result.int_value);
-      break;
-    case STRING_RESULT:
-      printf("%s", result.string_value);
-      break;
-    }
-    result.isThereReturn = false;
+    print_result(resultleft);
+    break;
+  case O_SUB:
+    sub = true;
   case O_ADD: {
-    interpretResult resultleft = interpret_ast(tree->left);
-    interpretResult resultright = interpret_ast(tree->right);
-    resultType lefttype = resultleft.resulttype;
-    resultType righttype = resultright.resulttype;
-    if (righttype != STRING_RESULT && lefttype != STRING_RESULT) {
-      double add_result = lefttype == INT_RESULT ? resultleft.int_value
-                          : resultleft.double_value + righttype == INT_RESULT
-                              ? resultright.int_value
-                              : resultright.double_value;
-      result.resulttype =
-          (((int)add_result) == add_result) ? INT_RESULT : DOUBLE_RESULT;
-      if (result.resulttype == INT_RESULT) {
-        result.int_value = (int)add_result;
-      } else {
-        result.double_value = add_result;
-      }
-    } else if (righttype == STRING_RESULT && lefttype == STRING_RESULT) {
+    if (lefttype == righttype && lefttype == NUMBER) {
+      if (sub)
+        result.number = resultleft.number - resultright.number;
+      else
+        result.number = resultleft.number + resultright.number;
+      result.type = NUMBER;
+      result.isReturn = false;
+    } else if (righttype == lefttype && lefttype == STRING && sub == false) {
       // Concatenate strings
       char *string_result =
-          malloc(sizeof(char) * (strlen(resultright.string_value) +
-                                 strlen(resultleft.string_value) + 1));
-      strcpy(string_result, resultleft.string_value);
-      strcat(string_result, resultright.string_value);
-      result.resulttype = STRING_RESULT;
-      result.string_value = string_result;
+          malloc(sizeof(char) *
+                 (strlen(resultright.string) + strlen(resultleft.string) + 1));
+      strcpy(string_result, resultleft.string);
+      strcat(string_result, resultright.string);
+      result.type = STRING;
+      result.string = string_result;
     } else {
       // TODO: add type debugging
       fprintf(stderr, "Cannot add type ... and type ...\n");
@@ -446,8 +526,112 @@ interpretResult interpret_ast(ast *tree) {
     }
     break;
   }
+  case O_MUL: {
+    if (lefttype == righttype && lefttype == NUMBER) {
+      result.type = NUMBER;
+      result.number = resultleft.number * resultright.number;
+      result.isReturn = false;
+      break;
+    } else if ((lefttype == NUMBER && righttype == STRING) ||
+               (lefttype == STRING && righttype == NUMBER)) {
+      result.type = STRING;
+      char *string =
+          lefttype == NUMBER ? resultright.string : resultleft.string;
+      double times =
+          lefttype == NUMBER ? resultleft.number : resultright.number;
+      if (times < 0) {
+        fprintf(stderr,
+                "WARN : Cannot Multiply a string by a negative number !\n");
+        result.string = "";
+        break;
+      }
+      if (times != (int)times)
+        fprintf(stderr,
+                "WARN : Cannot multiply a string by %g, this number will "
+                "automatically be converted to %d\n",
+                times, (int)times);
+      times = (int)times;
+      char *result_string = malloc(sizeof(char) * (strlen(string) * times + 1));
+      result_string[0] = '\0';
+      for (int i = 0; i < times; ++i) {
+        strcat(result_string, string);
+      }
+      result.string = result_string;
+    } else {
+      // TODO: add type debugging
+      fprintf(stderr, "Cannot add type ... and type ...\n");
+      exit(1);
+    }
+    break;
+  }
+  case O_DIV:
+    if (lefttype != NUMBER || righttype != NUMBER) {
+      // TODO: add type debugging
+      fprintf(stderr, "Cannot divide type ... by type ...\n");
+      exit(1);
+    }
+    result.type = NUMBER;
+    result.number = resultleft.number / resultright.number;
+    break;
+  case O_NEGATE:
+    result = resultleft;
+    result.number = -result.number;
+    break;
+
+  default:
+    fprintf(stderr, "WARN : op not implemented, skipped.\n");
   }
   return result;
+}
+
+void print_result(interpretResult result) {
+  switch (result.type) {
+  case TUPLE:
+    print_result(interpret_ast(result.tuple_left));
+    print_result(interpret_ast(result.tuple_right));
+    break;
+  case NUMBER:
+    printf("%g", result.number);
+    break;
+  case STRING: {
+    int len = strlen(result.string);
+    for (int i = 0; i < len; ++i) {
+      switch (result.string[i]) {
+      case '\\':
+        ++i;
+        switch (result.string[i]) {
+        case '\\':
+          putchar('\\');
+          break;
+        case 'n':
+          putchar('\n');
+          break;
+        case 't':
+          putchar('\t');
+          break;
+        default:
+          fprintf(stderr, " WARN : Unrecognized escape sequence : \\%c\n",
+                  result.string[i]);
+        }
+        break;
+      default:
+        putchar(result.string[i]);
+        break;
+      }
+    }
+    return;
+  }
+  }
+}
+void print_ast(ast *tree, int depth) {
+  if (tree == NULL)
+    return;
+  for (int i = 0; i < depth; ++i) {
+    printf("  ");
+  }
+  printf("%d\n", tree->op);
+  print_ast(tree->left, depth + 1);
+  print_ast(tree->right, depth + 1);
 }
 
 int main(int argc, char **argv) {
@@ -464,7 +648,8 @@ int main(int argc, char **argv) {
   parser->current_token = &token_list[0];
   parser->next_token = &token_list[1];
   parser->i = 0;
-  ast *tree = statement();
+  ast *tree = compound_statement(true);
+  /* print_ast(tree, 0); */
   interpret_ast(tree);
   free_ast(tree);
   return 0;
